176c99aff5ad7357408473150f4c39b3681cbb3e
nzapisocki <nzapisoc@ualberta.ca>
2026-03-05 20:08:47 -0700

    Added methods and tests

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -21,4 +21,28 @@ public class CustomList {
     public boolean hasCity(City city) {
         return cities.contains(city);
     }
+
+    /**
+     * Deletes a city from the list
+     * @param city
+     * The city to be deleted
+     */
+    public void delete(City city) {
+        if (cities.contains(city)){
+            cities.remove(city);
+        }
+        else
+        {
+            throw new IllegalArgumentException("City is not in the list");
+        }
+
+    }
+    /**
+     * Gets the count of the cities
+     * @return
+     * city count
+     */
+    public int countCities(){
+        return cities.size();
+    }
 }
--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -11,4 +11,32 @@ public class CustomListTest {
         // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
+
+    @Test
+    public void testDeleteCity(){
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        // test if Calgary is added
+        assertTrue(list.hasCity(calgary));
+        // remove calgary
+        list.delete(calgary);
+        // test to see if Calgary is removed
+        assertFalse(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+        City balzac = new City("Balzac", "AB");
+        City morinville = new City("Morinville", "AB");
+        list.addCity(calgary);
+        list.addCity(edmonton);
+        list.addCity(balzac);
+        list.addCity(morinville);
+        // This will fail initially because hasCity() doesn't exist
+        assertEquals(4, list.countCities());
+    }
 }
